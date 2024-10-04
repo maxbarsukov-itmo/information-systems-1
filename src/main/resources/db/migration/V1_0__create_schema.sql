7b56176df0fa8a0a571c2f0f141b90f6d7dc6daf
maxbarsukov <maximbarsukov@bk.ru>
2024-10-04 22:31:31 +0300

    fix: :bug: fix role naming for sprign security

--- src/main/resources/db/migration/V1_0__create_schema.sql
+++ src/main/resources/db/migration/V1_0__create_schema.sql
@@ -7,14 +7,14 @@ CREATE TYPE dragon_type AS ENUM ('WATER', 'UNDERGROUND', 'AIR', 'FIRE');
 CREATE TYPE color AS ENUM ('GREEN', 'RED', 'BLACK', 'YELLOW', 'BROWN');
 
 -- Вспомогательный тип для ролей пользователей (enum Role)
-CREATE TYPE user_role AS ENUM ('USER', 'ADMIN');
+CREATE TYPE user_role AS ENUM ('ROLE_USER', 'ROLE_ADMIN');
 
 -- Таблица пользователей системы
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,                                      -- Уникальный идентификатор пользователя
   username VARCHAR(255) NOT NULL UNIQUE,                      -- Логин пользователя (уникальный)
   password_hash VARCHAR(128) NOT NULL UNIQUE,                 -- Хэш пароля (используется SHA-512)
-  role user_role NOT NULL,                                    -- Роль пользователя: 'USER', 'ADMIN'
+  role user_role NOT NULL,                                    -- Роль пользователя: 'ROLE_USER', 'ROLE_ADMIN'
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()  -- Дата создания пользователя
 );
 
