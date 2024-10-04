63853077f661b663e4cf3469ea02e2c44c84160d
maxbarsukov <maximbarsukov@bk.ru>
2024-10-04 22:16:40 +0300

    test: :rotating_light: ok adding that one test back

---
+++ src/test/java/ru/ifmo/is/lab1/Lab1ApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.ifmo.is.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Lab1ApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
