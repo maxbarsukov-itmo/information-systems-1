69b6a6ce91b93e917673492d88a9afd08f48e25b
maxbarsukov <maximbarsukov@bk.ru>
2024-10-19 00:46:49 +0300

    refactor: :recycle: make crud controller generator

---
+++ src/main/java/ru/ifmo/is/lab1/common/framework/CrudController.java
@@ -0,0 +1,81 @@
+package ru.ifmo.is.lab1.common.framework;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import ru.ifmo.is.lab1.common.framework.dto.AuditableDto;
+import ru.ifmo.is.lab1.common.search.SearchDto;
+
+@AllArgsConstructor
+public abstract class CrudController<
+  T extends Auditable,
+  TDto extends AuditableDto,
+  TCreateDto,
+  TUpdateDto,
+  TService extends CrudService<T, ?, ?, ?, TDto, TCreateDto, TUpdateDto>
+  > {
+
+  private TService service;
+
+  @GetMapping
+  @Operation(summary = "Получить все объекты")
+  public ResponseEntity<Page<TDto>> index(@PageableDefault(size = 20) Pageable pageable) {
+    var objs = service.getAll(pageable);
+    return ResponseEntity.ok()
+      .header("X-Total-Count", String.valueOf(objs.getTotalElements()))
+      .body(objs);
+  }
+
+  @PostMapping("/search")
+  @Operation(summary = "Поиск и фильтрация объектов")
+  public ResponseEntity<Page<TDto>> search(
+    @PageableDefault(size = 20) Pageable pageable,
+    @RequestBody(required = false) SearchDto request
+  ) {
+    var objs = service.findBySearchCriteria(request, pageable);
+    return ResponseEntity.ok()
+      .header("X-Total-Count", String.valueOf(objs.getTotalElements()))
+      .body(objs);
+  }
+
+  @GetMapping("/{id}")
+  @Operation(summary = "Получить объект по ID")
+  public ResponseEntity<TDto> show(@PathVariable int id) {
+    var obj = service.getById(id);
+    return ResponseEntity.ok(obj);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+  @Operation(summary = "Создать объект")
+  public ResponseEntity<TDto> create(@Valid @RequestBody TCreateDto request) {
+    var obj = service.create(request);
+    return ResponseEntity.ok(obj);
+  }
+
+  @PutMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+  @Operation(summary = "Обновить объект по ID")
+  public ResponseEntity<TDto> update(@PathVariable int id, @Valid @RequestBody TUpdateDto request) {
+    var obj = service.update(request, id);
+    return ResponseEntity.ok(obj);
+  }
+
+  @DeleteMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+  @Operation(summary = "Удалить объект по ID")
+  public ResponseEntity<Void> delete(@PathVariable int id) {
+    if (service.delete(id)) {
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
+  }
+}
--- src/main/java/ru/ifmo/is/lab1/locations/LocationController.java
+++ src/main/java/ru/ifmo/is/lab1/locations/LocationController.java
@@ -1,82 +1,26 @@
 package ru.ifmo.is.lab1.locations;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import ru.ifmo.is.lab1.common.search.SearchDto;
-import ru.ifmo.is.lab1.locations.dto.LocationCreateDto;
-import ru.ifmo.is.lab1.locations.dto.LocationUpdateDto;
-import ru.ifmo.is.lab1.locations.dto.LocationDto;
-import jakarta.validation.Valid;
+import ru.ifmo.is.lab1.common.framework.CrudController;
+import ru.ifmo.is.lab1.locations.dto.*;
 
 @RestController
 @RequestMapping(value = "/api/locations", produces = MediaType.APPLICATION_JSON_VALUE)
-@RequiredArgsConstructor
 @Tag(name = "Location")
-public class LocationController {
+public class LocationController
+  extends CrudController<
+    Location,
+    LocationDto,
+    LocationCreateDto,
+    LocationUpdateDto,
+    LocationService
+  > {
 
-  private final LocationService service;
-
-  @GetMapping
-  @Operation(summary = "Получить все локации")
-  public ResponseEntity<Page<LocationDto>> index(@PageableDefault(size = 20) Pageable pageable) {
-    var locations = service.getAll(pageable);
-    return ResponseEntity.ok()
-      .header("X-Total-Count", String.valueOf(locations.getTotalElements()))
-      .body(locations);
-  }
-
-  @PostMapping("/search")
-  @Operation(summary = "Поиск и фильтрация локаций")
-  public ResponseEntity<Page<LocationDto>> search(
-    @PageableDefault(size = 20) Pageable pageable,
-    @RequestBody(required = false) SearchDto request
+  public LocationController(
+    LocationService service
   ) {
-    var locations = service.findBySearchCriteria(request, pageable);
-    return ResponseEntity.ok()
-      .header("X-Total-Count", String.valueOf(locations.getTotalElements()))
-      .body(locations);
-  }
-
-  @GetMapping("/{id}")
-  @Operation(summary = "Получить локацию по ID")
-  public ResponseEntity<LocationDto> show(@PathVariable int id) {
-    var location = service.getById(id);
-    return ResponseEntity.ok(location);
-  }
-
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-  @Operation(summary = "Создать локацию")
-  public ResponseEntity<LocationDto> create(@Valid @RequestBody LocationCreateDto request) {
-    var location = service.create(request);
-    return ResponseEntity.ok(location);
-  }
-
-  @PutMapping("/{id}")
-  @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-  @Operation(summary = "Обновить локацию по ID")
-  public ResponseEntity<LocationDto> update(@PathVariable int id, @Valid @RequestBody LocationUpdateDto request) {
-    var location = service.update(request, id);
-    return ResponseEntity.ok(location);
-  }
-
-  @DeleteMapping("/{id}")
-  @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-  @Operation(summary = "Удалить локацию по ID")
-  public ResponseEntity<Void> delete(@PathVariable int id) {
-    if (service.delete(id)) {
-      return ResponseEntity.noContent().build();
-    }
-    return ResponseEntity.notFound().build();
+    super(service);
   }
 }
