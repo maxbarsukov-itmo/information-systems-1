db5e1850a02674223fd24648bb646eff1180f332
maxbarsukov <maximbarsukov@bk.ru>
2024-11-02 00:26:35 +0300

    test: :rotating_light: add basic Application test

--- src/test/java/ru/ifmo/is/lab1/Lab1ApplicationTests.java
+++ src/test/java/ru/ifmo/is/lab1/Lab1ApplicationTests.java
@@ -1,11 +1,34 @@
 package ru.ifmo.is.lab1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Lab1Application")
 public class Lab1ApplicationTests {
+
+  transient MockedStatic<SpringApplication> springApplicationMock;
+
+  @BeforeAll
+  void beforeAll() {
+    springApplicationMock = mockStatic(SpringApplication.class);
+  }
+
   @Test
   void contextLoads() {
+    springApplicationMock
+      .when(() -> SpringApplication.run(Lab1Application.class, new String[] {}))
+      .thenCallRealMethod();
+
+    Lab1Application.main(new String[] {});
+
+    springApplicationMock.verify(() -> SpringApplication.run(Lab1Application.class, new String[] {}), times(1));
   }
 }
