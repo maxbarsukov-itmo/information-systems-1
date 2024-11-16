f271413cfc8db339c3f7259ed37fcfdf9171606e
maxbarsukov <maximbarsukov@bk.ru>
2024-11-16 07:43:24 +0300

    build: :building_construction: add cracked MUI X License checker bundles

---
+++ frontend/internal/scripts/after_deps_install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+yes | cp -f ./internal/scripts/@mui/x-license-pro/dist/cjs/index.js ./node_modules/@mui/x-license-pro/dist/cjs/index.js
+yes | cp -f ./internal/scripts/@mui/x-license-pro/dist/esm/index.js ./node_modules/@mui/x-license-pro/dist/esm/index.js
