07d5da26a3dc4b736cf536c5a2a5a4f776fc20ba
Даниил Горляков <63161448+pmpknu@users.noreply.github.com>
2024-11-22 20:10:33 +0000

    feat(frontend-adminrequests): add admin requests store

--- frontend/src/store/admin/index.ts
+++ frontend/src/store/admin/index.ts
@@ -1,15 +1,31 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction, Draft } from '@reduxjs/toolkit';
 import AdminRequestService from 'services/api/AdminRequestService';
 import { AdminRequestDto } from 'interfaces/dto/adminrequests/AdminRequestDto';
 import Paged from 'interfaces/models/Paged';
-import { extractApiError } from 'interfaces/crud/CrudSlice';
+import { extractApiError, rtkErrorToApiError } from 'interfaces/crud/CrudSlice';
+import Error from 'interfaces/crud/Error';
+import ApiError from 'interfaces/errors/ApiError';
 
 const initialState = {
   data: null as Paged<AdminRequestDto> | null,
   pendingData: null as Paged<AdminRequestDto> | null,
   singleData: null as AdminRequestDto | null,
-  loading: false,
-  error: null as string | null,
+  loading: {
+    fetch: false,
+    get: false,
+    create: false,
+    update: false,
+    delete: false,
+    search: false,
+  },
+  error: {
+    fetch: null,
+    get: null,
+    create: null,
+    update: null,
+    delete: null,
+    search: null,
+  } as Error,
 };
 
 export const fetchAdminRequests = createAsyncThunk(
@@ -71,3 +87,88 @@ export const processAdminRequest = createAsyncThunk(
     }
   }
 );
+
+const adminRequestsSlice = createSlice({
+  name: 'adminRequests',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // fetchAll
+      .addCase(fetchAdminRequests.pending, (state) => {
+        state.loading.fetch = true;
+        state.error.fetch = null;
+      })
+      .addCase(fetchAdminRequests.fulfilled, (state, action: PayloadAction<Paged<AdminRequestDto>>) => {
+        state.loading.fetch = false;
+        state.error.fetch = null;
+        state.data = action.payload;
+      })
+      .addCase(fetchAdminRequests.rejected, (state, action) => {
+        state.loading.fetch = false;
+        state.error.fetch = action.payload as ApiError || rtkErrorToApiError(action.error);
+      })
+
+      // fetchPending
+      .addCase(fetchPendingAdminRequests.pending, (state) => {
+        state.loading.fetch = true;
+        state.error.fetch = null;
+      })
+      .addCase(fetchPendingAdminRequests.fulfilled, (state, action: PayloadAction<Paged<AdminRequestDto>>) => {
+        state.loading.fetch = false;
+        state.error.fetch = null;
+        state.pendingData = action.payload;
+      })
+      .addCase(fetchPendingAdminRequests.rejected, (state, action) => {
+        state.loading.fetch = false;
+        state.error.fetch = action.payload as ApiError || rtkErrorToApiError(action.error);
+      })
+
+      // fetchSingle
+      .addCase(fetchAdminRequest.pending, (state) => {
+        state.loading.get = true;
+        state.error.get = null;
+      })
+      .addCase(fetchAdminRequest.fulfilled, (state, action: PayloadAction<AdminRequestDto>) => {
+        state.loading.get = false;
+        state.error.get = null;
+        state.singleData = action.payload;
+      })
+      .addCase(fetchAdminRequest.rejected, (state, action) => {
+        state.loading.get = false;
+        state.error.get = action.payload as ApiError || rtkErrorToApiError(action.error);
+      })
+
+      // create
+      .addCase(createAdminRequest.pending, (state) => {
+        state.loading.create = true;
+        state.error.create = null;
+      })
+      .addCase(createAdminRequest.fulfilled, (state, action: PayloadAction<AdminRequestDto>) => {
+        state.loading.create = false;
+        state.error.create = null;
+        state.singleData = action.payload;
+      })
+      .addCase(createAdminRequest.rejected, (state, action) => {
+        state.loading.create = false;
+        state.error.create = action.payload as ApiError || rtkErrorToApiError(action.error);
+      })
+
+      // process
+      .addCase(processAdminRequest.pending, (state) => {
+        state.loading.update = true;
+        state.error.update = null;
+      })
+      .addCase(processAdminRequest.fulfilled, (state, action: PayloadAction<AdminRequestDto>) => {
+        state.loading.update = false;
+        state.error.update = null;
+        state.singleData = action.payload;
+      })
+      .addCase(processAdminRequest.rejected, (state, action) => {
+        state.loading.update = false;
+        state.error.update = action.payload as ApiError || rtkErrorToApiError(action.error);
+      });
+  },
+});
+
+export default adminRequestsSlice.reducer;
